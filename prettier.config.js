0f525bbff7a08e2e2c441eedfec2c938b28d1bca
Vishal Choudhary <contactvishaltech@gmail.com>
2022-02-28 23:15:18 +0530

    setting up eslint and prettier

---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+const options = {
+  arrowParens: "avoid",
+  singleQuotes: true,
+  bracketSpacing: true,
+  endOfLine: "lf",
+  semi: false,
+  tabWidth: 2,
+  trailingCommas: "none",
+}
+
+module.exports = options
